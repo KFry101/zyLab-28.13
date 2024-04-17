cf22e3c7eba8b2ce8e7e7c8fcb46142a511e9ebb
Lu-Rin-e <167129812+Lu-Rin-e@users.noreply.github.com>
2024-04-17 13:49:58 -0500

    Create ItemToPurchase.h
    yah

---
+++ ItemToPurchase.h
@@ -0,0 +1,24 @@
+#ifndef ITEM_TO_PURCHASE_H
+#define ITEM_TO_PURCHASE_H
+
+#include <string>
+using namespace std;
+
+/* Type your code here */
+ItemToPurchase() {
+  private:
+
+    string itemName; //Initialized in default constructor to "none"
+    int itemPrice; //Initialized in default constructor to 0
+    int itemQuantity; //Initialized in default constructor to 0
+  public:
+    ItemToPurchase();
+    ItemToPurchase(string itemName, int itemPrice, int itemQuantity);
+    void SetName(string name);
+    string GetName();
+    void SetPrice(int price);
+    int GetPrice();
+    void SetQuantity(int quantity);
+    int GetQuantity();
+}
+#endif
